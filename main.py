35e21371a8d152037f6cc1c211718c9414e8060d
&! Ѵιchy.#0110 <Its_Vichy@protonmail.com>
2022-05-24 13:30:06 +0200

    leave star

---
+++ main.py
@@ -0,0 +1,34 @@
+import socket, threading, httpx
+
+class Server(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('0.0.0.0', 1337))
+        s.listen(1)
+
+        while True:
+            conn, _ = s.accept()
+            data = conn.recv(1024)
+            print(f'[SOCKET] {data}')
+            break
+
+if __name__ == '__main__':
+    Server().start()
+
+    try:
+        # Build your requests and send it to "http://127.0.0.1:1337".
+
+        payload = {
+            "content": "wow !"
+        }
+
+        header = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
+        }
+
+        httpx.post('http://127.0.0.1:1337', json=payload, headers=header)
+    except:
+        pass
